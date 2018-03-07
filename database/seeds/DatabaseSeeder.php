d41fe1f08cc70ecea95d3f5bb30785b69f69d8a7
Erico79 <emurinyo@gmail.com>
2018-03-07 16:25:42 +0300

    Collect Information about the Business Owner Registration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \Illuminate\Support\Facades\DB::transaction(function(){
-//            $this->call(MasterfileTableSeeder::class);
-//            $this->call(RoleTableSeeder::class);
-//            $this->call(UserTableSeeder::class);
+            $this->call(MasterfileTableSeeder::class);
+            $this->call(RoleTableSeeder::class);
+            $this->call(UserTableSeeder::class);
             $this->call(RoutesTableSeeder::class);
 //            $this->call(CommitteeTableSeeder::class);
 //            $this->call(BroadcastTypeTableSeeder::class);
@@ -25,8 +25,8 @@ class DatabaseSeeder extends Seeder
 //            $this->call(PlenarySittingSeeder::class);
 //            $this->call(FontAwesomeSeeder::class);
 //            $this->call(CommitteeDocsCatSeeder::class);
-            $this->call(CategoriesSeeder::class);
-            $this->call(CurrencySeeder::class);
+//            $this->call(CategoriesSeeder::class);
+//            $this->call(CurrencySeeder::class);
         });
 
     }
